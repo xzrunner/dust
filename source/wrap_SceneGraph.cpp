f343cb2f1c0214a1b82314ae477735e80ff94f4a
zz <zhuguang@ejoy.com>
2018-03-20 05:58:59 +0800

    [ADDED] work dir

--- source/wrap_SceneGraph.cpp
+++ source/wrap_SceneGraph.cpp
@@ -5,18 +5,24 @@
 #include "dust/Context.h"
 #include "dust/SceneNode.h"
 
+#include <boost/filesystem.hpp>
+
 namespace dust
 {
 
-#define INSTANCE() (Blackboard::Instance()->ctx->GetModuleMgr().GetModule<SceneGraph>(Module::M_SCENE_GRAPH))
+#define INSTANCE() (Blackboard::Instance()->GetContext()->GetModuleMgr().GetModule<SceneGraph>(Module::M_SCENE_GRAPH))
 
 int w_new_scene_node(lua_State* L)
 {
 	const char* filepath = luaL_checkstring(L, 1);
+	auto work_dir = Blackboard::Instance()->GetContext()->GetWorkDir();
+	auto real_path = boost::filesystem::absolute(filepath, work_dir);
+
 	SceneNode* node = nullptr;
-	luax_catchexcept(L, [&]() { node = new SceneNode(filepath); });
+	luax_catchexcept(L, [&]() { node = new SceneNode(real_path.string()); });
 	luax_pushtype(L, SCENE_NODE_ID, node);
 	node->Release();
+
 	return 1;
 }
 
