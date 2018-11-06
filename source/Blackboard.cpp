f1b9b00ea364aa6ff1154ef552a95e15b162875c
zz <xzrunner@gmail.com>
2018-11-06 14:11:27 +0800

    [FIXED] added macro EASYEDITOR

--- source/Blackboard.cpp
+++ source/Blackboard.cpp
@@ -4,7 +4,9 @@
 #include <ee0/WxStagePage.h>
 #include <ee0/WxStageCanvas.h>
 
+#ifdef EASYEDITOR
 #include <wx/window.h>
+#endif // EASYEDITOR
 
 namespace moon
 {
@@ -17,11 +19,15 @@ Blackboard::Blackboard()
 
 sm::vec2 Blackboard::GetMousePos() const
 {
+#ifdef EASYEDITOR
 	const wxPoint pt = wxGetMousePosition();
 	int mouse_x = pt.x - m_stage->GetScreenPosition().x;
 	int mouse_y = pt.y - m_stage->GetScreenPosition().y;
 	return ee0::CameraHelper::TransPosScreenToProject(
 		*m_stage->GetImpl().GetCanvas()->GetCamera(), mouse_x, mouse_y);
+#else
+	return sm::vec2();
+#endif // EASYEDITOR
 }
 
 }
